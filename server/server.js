cdab7736300d49d4aadbe8dd534b351b1b2bce5e
Jordan Bice <jmbice@gmail.com>
2019-04-04 10:25:09 -0700

    Serve static files, allow for cross-origin-resource-sharing

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const path = require('path');
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
