38868ac992e05f608d6159f4d81bb15e9d7d0db5
Jordan Bice <jmbice@gmail.com>
2019-04-04 14:25:56 -0700

    Add search component, update method. Rename searchQuery to fetchQuery.

--- client/components/root.jsx
+++ client/components/root.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SearchList from './SearchList';
+import Search from './Search';
 
 class Root extends React.Component {
   constructor(props) {
     super(props);
-    this.searchQuery = this.searchQuery.bind(this);
+    this.fetchQuery = this.fetchQuery.bind(this);
+    this.updateSearchTerm = this.updateSearchTerm.bind(this);
     this.state = {
-      searchTerm: 'London',
+      searchTerm: '',
       searchResults: [],
       currentId: null,
       currentWeather: [],
@@ -15,29 +17,42 @@ class Root extends React.Component {
   }
 
   componentDidMount() {
-    this.searchQuery();
+    this.fetchQuery();
   }
 
-  searchQuery() {
+  fetchQuery(e) {
     const { searchTerm } = this.state;
+    if (searchTerm.length === 0) { return; }
+
     fetch(`/location/search/query/${searchTerm}`)
       .then(res => res.json())
       .then((d) => {
         this.setState({
           searchResults: [d],
+          searchTerm: '',
         });
       });
   }
 
+  updateSearchTerm(e) {
+    this.setState({ searchTerm: e.target.value });
+  }
 
   render() {
-    const { searchResults } = this.state;
-
-    console.log(searchResults);
+    const { searchResults, searchTerm } = this.state;
 
     return (
       <div>
-        <SearchList searchResults={searchResults} />
+        <div>
+          <Search
+            update={this.updateSearchTerm}
+            initiate={this.fetchQuery}
+            term={searchTerm}
+          />
+        </div>
+        <div>
+          <SearchList searchResults={searchResults} />
+        </div>
       </div>
     );
   }
