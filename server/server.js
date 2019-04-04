3c4159e5d315049b49e96c961d83e736e62d7939
Jordan Bice <jmbice@gmail.com>
2019-04-04 12:23:29 -0700

    Write API calls to be made on server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path');
+const request = require('request');
 
 const app = express()
 const port = 3000
@@ -12,5 +13,59 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+
+app.get('/location/search/query', (req, res) => {
+  //get location from name
+  request(`www.metaweather.com/api/location/search/?query=${req.params.query}`, (error, response, body) => {
+    if (error) {
+      response
+        ? res.status(response.statusCode).send()
+        : console.log('error getting content');
+    } else {
+      res.status(response.statusCode).send(body);
+    }
+  });
+});
+
+app.get('/location/search/lattlong', (req, res) => {
+  // location from latt/long
+  request(`www.metaweather.com/api/location/search/?lattlong=${req.params.latt, req.params.long}`, (error, response, body) => {
+    if (error) {
+      response
+        ? res.status(response.statusCode).send()
+        : console.log('error getting content');
+    } else {
+      res.status(response.statusCode).send(body);
+    }
+  });
+});
+
+app.get('/location/weather/today/', (req, res) => {
+  // weather from id, today
+  request(`www.metaweather.com/api/location/${eq.params.woeid}/`, (error, response, body) => {
+    if (error) {
+      response
+        ? res.status(response.statusCode).send()
+        : console.log('error getting content');
+    } else {
+      res.status(response.statusCode).send(body);
+    }
+  });
+});
+
+
+app.get('/location/weather/date', (req, res) => {
+  // weather from id, on date
+  request(`www.metaweather.com/api/location/${eq.params.woeid}/${eq.params.date}/`, (error, response, body) => {
+    if (error) {
+      response
+        ? res.status(response.statusCode).send()
+        : console.log('error getting content');
+    } else {
+      res.status(response.statusCode).send(body);
+    }
+  });
+});
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
