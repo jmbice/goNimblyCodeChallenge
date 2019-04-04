b944eed91867ca8d84d7465eb823841ea2dd578f
Jordan Bice <jmbice@gmail.com>
2019-04-04 13:46:41 -0700

    Add ajax call as method on root. Pass results of call to state, children

--- client/components/root.jsx
+++ client/components/root.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import SearchList from './SearchList';
 
 class Root extends React.Component {
   constructor(props) {
     super(props);
+    this.searchQuery = this.searchQuery.bind(this);
     this.state = {
-      searchTerm: '',
+      searchTerm: 'London',
       searchResults: [],
       currentId: null,
       currentWeather: [],
@@ -12,10 +14,30 @@ class Root extends React.Component {
     };
   }
 
+  componentDidMount() {
+    this.searchQuery();
+  }
+
+  searchQuery() {
+    const { searchTerm } = this.state;
+    fetch(`/location/search/query/${searchTerm}`)
+      .then(res => res.json())
+      .then((d) => {
+        this.setState({
+          searchResults: [d],
+        });
+      });
+  }
+
+
   render() {
+    const { searchResults } = this.state;
+
+    console.log(searchResults);
+
     return (
       <div>
-        Hello again
+        <SearchList searchResults={searchResults} />
       </div>
     );
   }
