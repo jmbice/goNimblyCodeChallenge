811ebfd94a9f242993905ab19db8951a15810efe
Jordan Bice <jmbice@gmail.com>
2019-04-04 13:50:45 -0700

    Reduce scope of API calls, correct formating mistakes

--- server/server.js
+++ server/server.js
@@ -14,35 +14,48 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/location/search/query', (req, res) => {
+app.get('/location/search/query/:searchTerm', (req, res) => {
   //get location from name
-  request(`www.metaweather.com/api/location/search/?query=${req.params.query}`, (error, response, body) => {
+  request(`https://www.metaweather.com/api/location/search/?query=${req.params.searchTerm}`, (error, response, body) => {
     if (error) {
       response
         ? res.status(response.statusCode).send()
-        : console.log('error getting content');
+        : console.log('error getting search term results');
     } else {
-      res.status(response.statusCode).send(body);
+      const searchResults = JSON.parse(body);
+      if(searchResults.length > 1) {
+        res.status(response.statusCode).send(body);
+      } else {
+        request(`https://www.metaweather.com/api/location/${searchResults[0].woeid}/`, (e, r, b) => {
+          if (e) {
+            r
+              ? res.status(r.statusCode).send()
+              : console.log('error getting weather results from search term');
+          } else {
+            res.status(r.statusCode).send(b);
+          }
+        });
+      }
     }
   });
 });
 
-app.get('/location/search/lattlong', (req, res) => {
-  // location from latt/long
-  request(`www.metaweather.com/api/location/search/?lattlong=${req.params.latt, req.params.long}`, (error, response, body) => {
-    if (error) {
-      response
-        ? res.status(response.statusCode).send()
-        : console.log('error getting content');
-    } else {
-      res.status(response.statusCode).send(body);
-    }
-  });
-});
-
-app.get('/location/weather/today/', (req, res) => {
+// app.get('/location/search/lattlong', (req, res) => {
+//   // location from latt/long
+//   request(`www.metaweather.com/api/location/search/?lattlong=${req.params.latt, req.params.long}`, (error, response, body) => {
+//     if (error) {
+//       response
+//         ? res.status(response.statusCode).send()
+//         : console.log('error getting content');
+//     } else {
+//       res.status(response.statusCode).send(body);
+//     }
+//   });
+// });
+//
+app.get('/location/weather/today/:woeid', (req, res) => {
   // weather from id, today
-  request(`www.metaweather.com/api/location/${eq.params.woeid}/`, (error, response, body) => {
+  request(`www.metaweather.com/api/location/${req.params.woeid}/`, (error, response, body) => {
     if (error) {
       response
         ? res.status(response.statusCode).send()
@@ -52,20 +65,20 @@ app.get('/location/weather/today/', (req, res) => {
     }
   });
 });
-
-
-app.get('/location/weather/date', (req, res) => {
-  // weather from id, on date
-  request(`www.metaweather.com/api/location/${eq.params.woeid}/${eq.params.date}/`, (error, response, body) => {
-    if (error) {
-      response
-        ? res.status(response.statusCode).send()
-        : console.log('error getting content');
-    } else {
-      res.status(response.statusCode).send(body);
-    }
-  });
-});
+//
+//
+// app.get('/location/weather/date', (req, res) => {
+//   // weather from id, on date
+//   request(`www.metaweather.com/api/location/${req.params.woeid}/${eq.params.date}/`, (error, response, body) => {
+//     if (error) {
+//       response
+//         ? res.status(response.statusCode).send()
+//         : console.log('error getting content');
+//     } else {
+//       res.status(response.statusCode).send(body);
+//     }
+//   });
+// });
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
