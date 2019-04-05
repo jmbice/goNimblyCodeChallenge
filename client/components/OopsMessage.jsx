44745ca1514cba690a6e4351de8a58a9a95c6cb6
Jordan Bice <jmbice@gmail.com>
2019-04-05 02:29:03 -0700

    Add title & classNames to div in error message. Reword error message.

--- client/components/OopsMessage.jsx
+++ client/components/OopsMessage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const oopsMessage = () => (
-  <div>
+  <div className="oopsMessage-wrapper">
+    <div className="oopsMessage-title">
+      <h1>!</h1>
+    </div>
     <h4>
-      Uh oh, we do not have any data for what you inputed.
-      Check that you spelled the city correctly.
-      <br />
-      Please note, we only provide weather data for major cities.
+      Weather data not found.
+      Unfortunately, we only provide weather data for major cities.
       We may not have the data you are looking for.
     </h4>
   </div>
