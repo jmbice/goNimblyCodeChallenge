e5a26259f03d6be40859729757789fdca87f0392
Jordan Bice <jmbice@gmail.com>
2019-04-04 21:15:01 -0700

    Add button className so it highlights when input field is not empty.

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -4,14 +4,21 @@ const Search = (props) => {
   const { update, initiate, term } = props;
 
   return (
-    <div>
-        Enter a city:
+    <div className="search-wrapper">
+      <div className="search-instructions">
+        Enter the name of the city:
+      </div>
       <input
+        className="search-input"
         id="cityInput"
         value={term}
         onChange={update}
       />
-      <button type="button" onClick={initiate}>
+      <button
+        className={term.length ? 'search-button-ready' : 'search-button'}
+        type="button"
+        onClick={initiate}
+      >
         Get Weather
       </button>
     </div>
