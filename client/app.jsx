5fc5565b85a70cf0043fa37442fd60996e5e805d
Jordan Bice <jmbice@gmail.com>
2019-04-04 19:13:33 -0700

    Import scss styles into app

--- client/app.jsx
+++ client/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/Root';
+import './styles/_main.scss';
 
 ReactDOM.render(<Root />, document.getElementById('gn-entry'));
