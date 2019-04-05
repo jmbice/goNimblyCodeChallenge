82da4dff4ec9ae740992f6dbb113b0335412c22f
Jordan Bice <jmbice@gmail.com>
2019-04-05 09:25:01 -0700

    Test...

--- client/app.jsx
+++ client/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from './components/Root';
+import Root from './components/Root.jsx';
 import './styles/_main.scss';
 
 ReactDOM.render(<Root />, document.getElementById('gn-entry'));
